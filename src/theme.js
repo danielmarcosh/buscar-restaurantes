66dcb9e5bb5f15df0948fa4b7991f66f679eb3bc
Daniel Marcos <danielmarcosh@gmail.com>
2021-07-21 18:30:39 -0300

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#FC0FC0',
+    background: '#111111',
+    text: '#f5fffb',
+    success: '#00a900',
+  },
+  fonts: {
+    fontFamily: 'Zen Loop, Roboto, sans-serif',
+  },
+};
+
+export default theme;
