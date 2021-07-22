2fd0d6dbbee7b9943f3ab0c48394c312f23f211d
Daniel Marcos <danielmarcosh@gmail.com>
2021-07-22 19:28:44 -0300

    Inicio impl. do carousel p-2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
+
   return (
     <Wrapper>
       <Container>
@@ -19,6 +31,24 @@ const Home = () => {
             trailingIcon={<MaterialIcon role="button" icon="search" />}>
             <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           </TextField>
+          <CarouselTitle>Na sua área</CarouselTitle>
+          <Slider {...settings}>
+            <div>
+              <img src={restaurante} alt="" />
+            </div>
+            <div>
+              <img src={restaurante} alt="" />
+            </div>
+            <div>
+              <img src={restaurante} alt="" />
+            </div>
+            <div>
+              <img src={restaurante} alt="" />
+            </div>
+            <div>
+              <img src={restaurante} alt="" />
+            </div>
+          </Slider>
         </Search>
       </Container>
       <Map />
